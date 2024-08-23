2236385cc8773464010d912e86e48d80764c487e
omkar861856 <omkar861856@gmail.com>
2024-08-24 00:45:54 +0530

    layout changed -mui

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,14 +4,17 @@ import Home from './pages/Home';
 import MenuPage from './pages/MenuPage';
 import ReservationPage from './pages/ReservationPage';
 import './App.css';
+import PageNotFound from './pages/PageNotFound';
 
 const App = () => (
   
     <div className="app-container">
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/home" element={<Home />} />
         <Route path="/menu" element={<MenuPage />} />
         <Route path="/reservation" element={<ReservationPage />} />
+        <Route path="*" element={<PageNotFound />} />
       </Routes>
     </div>
   
